e94e76598abc33c56a4332643ba4a7e96ac4137b
Jarver <masterlogdog@gmail.com>
2019-02-28 16:01:32 +0200

    add router

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const process = require('process');
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(require('./app/routes/routes'));
 app.listen(PORT, () => console.log(`server started on ${PORT} port`));
