ed1912d4d56bfdcaed6bfc1ec203925c3d231c9d
Jarver <masterlogdog@gmail.com>
2019-02-28 16:15:35 +0200

    add views

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const expresslayouts = require('express-ejs-layouts');
 const process = require('process');
 const PORT = process.env.PORT || 3000;
 const app = express();
+// EJS
+app.use(expresslayouts);
+app.set('view engine', 'ejs');
+// Routes
 app.use('/', require('./app/routes/routes'));
 app.use('/users', require('./app/routes/routes'));
 app.listen(PORT, () => console.log(`server started on ${PORT} port`));
