e135eb9307b8eb1e77149225c9fd95da0f71dddb
Jarver <masterlogdog@gmail.com>
2019-02-28 18:13:25 +0200

    add register validation

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -10,7 +10,24 @@ router.get('/users/login', (req, res) => {
     res.render('login');
 });
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('hello');
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+    // Check required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({msg: 'Please fill in all fields'});
+    }
+    // Check password match
+    if(password !== password2) {
+        errors.push({msg: 'Passwords do not match'});
+    }
+    if(password.length < 6) {
+        errors.push({msg: 'Password should be at least 6 characters'})
+    }
+    if(errors.length > 0) {
+        res.render('register', {errors, name, email, password, password2});
+    }
+    else {
+        res.send('pass');
+    }
 });
 module.exports = router; // export pointer on router
