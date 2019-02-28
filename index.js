6d35d421f236965e9f4c6db6f97151ec3934dd74
Jarver <masterlogdog@gmail.com>
2019-02-28 15:26:41 +0200

    add process module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const process = require('process');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server started on ${PORT} port`));
