5d1b67662c9deceb048d16de61647d435a0efa9c
Jarver <masterlogdog@gmail.com>
2019-03-01 15:55:08 +0200

    add flash messages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 const process = require('process');
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -15,7 +17,20 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 // BodyParser
 app.use(express.urlencoded({extended: false}));
-
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+// Connect Flash
+app.use(flash());
+// Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 // Routes
 app.use('/', require('./app/routes/routes'));
 app.use('/users', require('./app/routes/routes'));
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -53,6 +53,7 @@ router.post('/register', (req, res) => {
                     newUser.save()
                         .then((user) => {
                             console.log('user saved', user);
+                            req.flash('success_msg', 'You are now registered and can log in');
                             res.redirect('/users/login');
                         })
                         .catch(err => console.log(err));
